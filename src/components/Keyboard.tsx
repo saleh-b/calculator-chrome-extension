916dc64c091cd0bc37daa7a8d12001ab4150dbf8
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-04-22 18:27:25 +0400

    changing folder structure

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -4,82 +4,82 @@ function Keyboard() {
     <>
       <Grid container spacing={2}>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             7
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             8
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             9
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             C
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             4
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             5
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             6
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             +
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             3
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             2
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             1
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             -
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             0
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             X
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             ÷
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button fullWidth variant='contained'>
+          <Button fullWidth size='large' variant='contained'>
             =
           </Button>
         </Grid>
